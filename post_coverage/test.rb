d209f805e628867bf29c6396c33288d148174fa2
granluo <granluo@google.com>
2021-05-03 23:32:23 -0700

    Test new pull request.

--- post_coverage/test.rb
+++ post_coverage/test.rb
@@ -1,5 +1,6 @@
 require 'octokit'
 
-client = Octokit::Client.new(access_token: ENV["INPUT_ACCESS-TOKEN"])
-client.create_check_suite("granluo/issue_generations", "30ef35bc7780dd4e1a947f3b395eeea7d50a7fa3")
+client = Octokit::Client.new(access_token: ENV["INPUT_ACCESS_TOKEN"])
+#client.add_comment(REPO_NAME_WITH_OWNER, last_issue.number, NO_WORKFLOW_RUNNING_INFO)
+client.create_pull_request_comment("granluo/issue_generations",98,"testing","e35ac3de3cf8c0f5ec95736cd7566a1f144afe18","post_coverage/test.rb",4, {:side=>"RIGHT", :start_line=> 3, :line=> 5})
 print ("asdf")
